c042b8d1fedc2844967521f60ab5e9fb153f768b
PngWnA <pngwna@pngwna.net>
2023-05-30 13:36:17 +0900

    Problem #2: skeleton 기반으로 solver 작성중

--- 2/solver.py
+++ 2/solver.py
@@ -1,12 +1,19 @@
-# Model import
 from keras.models import model_from_json
+import nr6.speck as sp
+
+# 주어진 딥러닝 모델 로드
+print("[*] Loading 6-round neural distinguisher... ", end='', flush=True)
 arch = open('./nr6/arch_neural_distinguisher.json')
 json_arch = arch.read()
 nr6_speck_distinguisher = model_from_json(json_arch) 
 nr6_speck_distinguisher.load_weights('./nr6/weights_nr6_speck.h5') 
 nr6_speck_distinguisher.compile(optimizer='adam',loss='mse',metrics=['acc']) 
-# Model test
+print("Done.")
+
+# 작성중
 import nr6.speck as sp
-x_test, y_test = sp.make_train_data(10**6, 6)
+print("[*] Generating sample plaintext pair")
+x_test, y_test = sp.make_train_data(1, 6)
+print(x_test, y_test)
 results = nr6_speck_distinguisher.evaluate(x_test, y_test, batch_size=10000) 
 print('test loss, test_acc: ', results)
