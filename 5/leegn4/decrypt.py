dd284a8639bf0e1ea640434e9c6c758967a356f4
PngWnA <pngwna@pngwna.net>
2023-07-02 16:53:29 +0900

    Problem #5: 문제 해결

--- 5/leegn4/decrypt.py
+++ 5/leegn4/decrypt.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-import sys,scrypt,ascon
-# import itertools
+import scrypt
+import ascon
 
-'INCHEONJEJUKANSI31L-13R'
-
-ct=open('../1/enc_blueprint','rb').read()
+P = b'INCHEONJEJUKANSAI31L-13R'
+ct=open('./enc_blueprint','rb').read()
 salt=b'contest2023'
-data=scrypt.hash(password=sys.argv[1].encode(),salt=salt,N=0x10000,r=8,p=1,buflen=48)
+data=scrypt.hash(password=P,salt=salt,N=0x10000,r=8,p=1,buflen=48)
 key,nonce,ad=data[:16],data[16:32],data[32:]
 
 pt=ascon.ascon_decrypt(key=key,nonce=nonce,associateddata=ad,ciphertext=ct)
 
 if pt is not None:
     print('Success')
-    open('./blueprint','wb').write(pt)
+    open('./blueprint.pdf','wb').write(pt)
 
 # iata=[b'ICN',b'CJU',b'KIX',b'JFK']
 # icao=[b'RKSI',b'RKPC',b'RJBB',b'KJFK']
