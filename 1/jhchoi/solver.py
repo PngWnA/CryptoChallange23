bdf233e3464350e5f240f603c3e1f150969006bb
jhchoi0303 <zzzmilky@naver.com>
2023-07-19 14:57:04 +0900

    Problem #1: iteration 추가, eps, alpha 조정

--- 1/jhchoi/solver.py
+++ 1/jhchoi/solver.py
@@ -1,110 +1,125 @@
-
-import sys
-
-import torch
-import torch.nn as nn
-
-sys.path.insert(0, '..')
-
-
-from torchvision import models #loading model
-from utils import get_imagenet_data #loading model
-from robustbench.utils import clean_accuracy #loading model: A collection of robust models for RobustBench: a standardized adversarial robustness benchmark
-import numpy as np 
-from utils import imshow, get_pred
-
+import os
+import random
 
 import torch
-import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from abc import ABC, abstractmethod
+import requests
 
-class Attacker(ABC):
-    def __init__(self, model, config):
-        """
-        ## initialization ##
-        :param model: Network to attack
-        :param config : configuration to init the attack
-        """
-        self.config = config
-        self.model = model
-        self.clamp = (0,1)
-    
-    def _random_init(self, x):
-        x = x + (torch.rand(x.size(), dtype=x.dtype, device=x.device) - 0.5) * 2 * self.config['eps']
-        x = torch.clamp(x,*self.clamp)
-        return x
+from PIL import Image
 
-    def __call__(self, x,y):
-        x_adv = self.forward(x,y)
-        return x_adv
+import matplotlib.pyplot as plt
+
+# 모델, 테스트셋 준비
+# ResNet-18 준비
+model = torchvision.models.resnet18(weights='DEFAULT').eval()
+print(f"[*] Loaded pretrained ResNet-18.")
+
+# ResNet-18 분류 라벨
+url = 'https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json'
+labels = requests.get(url).json()
+print(f"[*] Loaded human readable output label mapping (total {len(labels)} labels)")
+
+# ImageNet 데이터 로드
+# 아래 폴더에서 임의의 사진 1개 로드
+TARGET_DIR = "dataset"
+files = os.listdir(TARGET_DIR)
+file = random.choice(files)
+img = Image.open(os.path.join(TARGET_DIR, file))
+print(f"[*] Loaded random image from ImageNet: {file}")
 
 
-class PGD(Attacker):
-    def __init__(self, model, config, target=None):
-        super(PGD, self).__init__(model, config)
-        self.target = target
+# 이미지 형태를 4차원 텐서로 변환
+# 이미지 normalization 후 공격 진행하고 싶은 경우 아래 변수 수정
+DO_NORMALIZE = False
+if DO_NORMALIZE:
+    MEAN = [0.485, 0.456, 0.406]
+    STD = [0.229, 0.224, 0.225]
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)]
+        )
+    print(f"[*] Preprocessing with Image -> ToTensor -> Normalize -> Tensor")
+else:
+    transform = transforms.Compose(
+        [transforms.ToTensor()]
+        )
+    print(f"[*] Preprocessing with Image -> ToTensor -> Tensor")
+image = transform(img).unsqueeze(0)
 
-    def forward(self, x, y):
-        """
-        :param x: Inputs to perturb
-        :param y: Ground-truth label
-        :param target : Target label 
-        :return adversarial image
-        """
-        x_adv = x.detach().clone()
-        if self.config['random_init'] :
-            x_adv = self._random_init(x_adv)
-        for _ in range(self.config['attack_steps']):
-            x_adv.requires_grad = True
-            self.model.zero_grad()
-            logits = self.model(x_adv) #f(T((x))
-            if self.target is None:
-                # Untargeted attacks - gradient ascent
-                
-                loss = F.cross_entropy(logits, y,  reduction="sum")
-                loss.backward()                      
-                grad = x_adv.grad.detach()
-                grad = grad.sign()
-                x_adv = x_adv + self.config['attack_lr'] * grad
-            else:
-                # Targeted attacks - gradient descent
-                assert self.target.size() == y.size()           
-                loss = F.cross_entropy(logits, self.target)
-                loss.backward()
-                grad = x_adv.grad.detach()
-                grad = grad.sign()
-                x_adv = x_adv - self.config['attack_lr'] * grad
+# 공격 전 분류값 추출
+prediction = model(image)
+values, indexes = torch.topk(prediction, 5)
+classifications = [labels[index] for index in indexes[0].tolist()]
+origin_top1 = classifications[0]
+print(f"[*] Original image is classified as '{origin_top1}'")
 
-            # Projection
-            x_adv = x + torch.clamp(x_adv - x, min=-self.config['eps'], max=self.config['eps'])
-            x_adv = x_adv.detach()
-            x_adv = torch.clamp(x_adv, *self.clamp)
+# PGD 공격 파라미터
+eps = 0.01
+alpha = 2/255
+random_start = True
 
-        return x_adv
+# PGD 공격 수행
+# 데이터 로드
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+image = image.to(device)
+_, target = torch.max(prediction, 1)
+target = target.to(device)
 
-attack_config = {
-    'eps' : 8.0/255.0, 
-    'attack_steps': 10,
-    'attack_lr': 2.0 / 255.0, 
-    'random_init': True, 
-}
+loss = torch.nn.CrossEntropyLoss()
 
-images, labels = get_imagenet_data()
-print('[Data loaded]') #loading data
+if random_start:
+    adv_image = image + torch.empty_like(image).uniform_(-eps, eps)
+    adv_image = torch.clamp(adv_image, 0, 1)
+    print(f"[*] Start image' = image + ε, (|ε| < {eps})")
+else:
+    adv_image = torch.clamp(image, 0, 1)
+    print(f"[*] Start image' = image + ε, (ε = 0)")
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu" #change device from 'cuda' to 'cuda or cpu'
-model = models.resnet18(pretrained=True).to(device).eval() #loads pretrained model 
-acc = clean_accuracy(model, images.to(device), labels.to(device))
-print('[Model loaded]') #loading model
-print('Acc: %2.2f %%'%(acc*100))
+ITER = 40
+for i in range(0, ITER):
+    adv_image.requires_grad = True
+    output = model(adv_image)
 
-attack = PGD(model, attack_config)
-print(attack)
-adversarial_image = attack(images, labels)
-idx = 0
-pre = get_pred(model, adversarial_image[idx:idx+1], device)
-imshow(adversarial_image[idx:idx+1], title="True:%d, Pre:%d"%(labels[idx], pre))
+    model.zero_grad()
+    cost = loss(output, target).to(device)
+    cost.backward()
+
+    attack_images = adv_image + alpha*adv_image.grad.sign()
+    eta = torch.clamp(attack_images - image, min=-eps, max=eps)
+    print(f"[*] η = ε + α * sign(image')")
+    adv_image = torch.clamp(image + eta, min=0, max=1).detach_()
+    print(f"[*] image* = image + η")
+
+perturbation = adv_image - image
+
+# 공격 후 분류값 추출
+prediction = model(adv_image)
+values, indexes = torch.topk(prediction, 5)
+classifications = [labels[index] for index in indexes[0].tolist()]
+adv_top1 = classifications[0]
+print(f"[*] Adversarial image is classified as '{adv_top1}'")
+
+# 공격 성공 검증
+if origin_top1 != adv_top1:
+    print(f"[+] PGD perturbation attack successful! ({origin_top1} != {adv_top1})")
+else:
+    print(f"[+] PGD perturbation attack failed! ({origin_top1} == {adv_top1})")
+
+# 원본, 노이즈, 공격 이미지 출력
+_, figures = plt.subplots(1, 3, figsize=(15, 5))
+
+figures[0].imshow(image.squeeze(0).permute(1, 2, 0).clamp(0, 1))
+figures[0].set_title(f"Original image: classified as {origin_top1}")
+
+MULTIPLIER = 50
+figures[1].imshow(perturbation.squeeze(0).permute(1, 2, 0).clip(0, 255) * MULTIPLIER)
+figures[1].set_title(f"Perturbation: amplified by x{MULTIPLIER}")
+
+figures[2].imshow(adv_image.squeeze(0).permute(1, 2, 0).clamp(0, 1))
+figures[2].set_title(f"Adversarial image: classified as {adv_top1}")
+
+plt.show()
